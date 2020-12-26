67007f525116c4075cd07aa17afb133c9c14a4e2
sleduardo20 <sl.eduardo20@gmail.com>
2020-12-26 11:00:40 -0300

    (styles)🎨: add styles content

--- src/components/Intro/styles.ts
+++ src/components/Intro/styles.ts
@@ -4,6 +4,21 @@ export const Container = styled.div`
   height: 100vh;
   width: 100%;
   position: relative;
+
+  &::before {
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 1) 0%,
+      rgba(0, 0, 0, 1) 20%,
+      rgba(0, 0, 0, 0) 40%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 5;
+  }
 `;
 
 export const Intro = styled.section`
@@ -26,7 +41,40 @@ export const Title = styled(Intro)`
   justify-content: center;
 `;
 
-export const Crawl = styled.div``;
-export const CrawlContent = styled.div``;
-export const EpisodeNumber = styled.div``;
-export const EpisodeTitle = styled.div``;
+export const Crawl = styled.section`
+  width: 90%;
+  height: 80rem;
+  font-size: 300%;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%) perspective(300px) rotateX(28deg);
+  transform-origin: 50% 100%;
+`;
+
+export const CrawlContent = styled.div`
+  position: absolute;
+  top: 100%;
+
+  p {
+    font-weight: 700;
+    line-height: 1.33;
+    margin-bottom: 4rem;
+    text-align: justify;
+  }
+`;
+
+export const EpisodeNumber = styled.h1`
+  font-weight: 600;
+  margin-bottom: 5rem;
+  text-align: center;
+`;
+
+export const EpisodeTitle = styled.h2`
+  font-size: 250%;
+  font-weight: 500;
+  line-height: 1;
+  margin-bottom: 7rem;
+  transform: scale(1, 1.5);
+  text-align: center;
+`;
